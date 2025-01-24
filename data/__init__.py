101b6f21f61ed91eff9b11545d81d80a27d88b28
degull <skytmdgus99@gmail.com>
2025-01-24 15:05:20 +0900

    koniq init add

--- data/__init__.py
+++ data/__init__.py
@@ -3,5 +3,6 @@ from data.dataset_live import LIVEDataset
 from data.dataset_csiq import CSIQDataset
 from data.dataset_tid2013 import TID2013Dataset
 from data.dataset_kadid10k import KADID10KDataset
+from data.dataset_koniq10k import KONIQ10KDataset
 from data.dataset_flive import FLIVEDataset
 from data.dataset_spaq import SPAQDataset
